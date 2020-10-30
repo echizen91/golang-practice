1367625eb06c4b2f3a5110646ea7e3301638126a
Darrel <darrel_cai@hotmail.com>
2020-10-30 16:43:26 +0800

    inverse b-tree recursively

---
+++ leetcode-easy/invert_binary_tree.go
@@ -0,0 +1,16 @@
+package easy
+
+/*
+Invert a binary tree.
+
+*/
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	left := invertTree(root.Left)
+	right := invertTree(root.Right)
+	root.Left, root.Right = right, left
+	return root
+}

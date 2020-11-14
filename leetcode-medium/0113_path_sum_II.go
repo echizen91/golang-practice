7b4168c44dd716b2e1352024eca0a68358bee713
Darrel <darrel_cai@hotmail.com>
2020-11-14 17:29:18 +0800

    recursion, not optimized

---
+++ leetcode-medium/0113_path_sum_II.go
@@ -0,0 +1,38 @@
+package medium
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func findAllPath(root *TreeNode, currentSet []int, currentCount int, sum int, result *[][]int) {
+	if root == nil {
+		if currentCount == sum {
+			cp := make([]int, len(currentSet))
+			copy(cp, currentSet)
+			*result = append(*result, cp)
+		}
+		return
+	}
+	currentCount += root.Val
+	currentSet = append(currentSet, root.Val)
+
+	if root.Left != nil {
+		findAllPath(root.Left, currentSet, currentCount, sum, result)
+	}
+	if root.Right != nil {
+		findAllPath(root.Right, currentSet, currentCount, sum, result)
+	}
+	if root.Left == nil && root.Right == nil {
+		findAllPath(root.Left, currentSet, currentCount, sum, result)
+	}
+}
+
+func pathSum(root *TreeNode, sum int) [][]int {
+	var result [][]int
+	if root != nil && sum != 0 {
+		findAllPath(root, []int{}, 0, sum, &result)
+	}
+	return result
+}
---
+++ leetcode-medium/0113_path_sum_II_test.go
@@ -0,0 +1,76 @@
+package medium
+
+import (
+	"reflect"
+	"testing"
+)
+
+/*
+	  5
+     / \
+    4   8
+   /   / \
+  11  13  4
+ /  \    / \
+7    2  5   1
+*/
+
+func Test_pathSum(t *testing.T) {
+	type args struct {
+		root *TreeNode
+		sum  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{
+			name: "test-case-1",
+			args: args{
+				root: &TreeNode{
+					Val: 5,
+					Left: &TreeNode{
+						Val: 4,
+						Left: &TreeNode{
+							Val: 11,
+							Left: &TreeNode{
+								Val: 7,
+							},
+							Right: &TreeNode{
+								Val: 2,
+							},
+						},
+					},
+					Right: &TreeNode{
+						Val: 8,
+						Left: &TreeNode{
+							Val: 13,
+						},
+						Right: &TreeNode{
+							Val: 4,
+							Left: &TreeNode{
+								Val: 5,
+							},
+							Right: &TreeNode{
+								Val: 1,
+							},
+						},
+					},
+				},
+				sum: 22,
+			},
+			want: [][]int{
+				{5, 4, 11, 2},
+				{5, 8, 4, 5},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := pathSum(tt.args.root, tt.args.sum); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("pathSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
